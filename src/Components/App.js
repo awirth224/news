64a12bf025bda2ab13e42b771412383c484b6c6a
Angela Wirth <angiewirth92@gmail.com>
2023-04-20 12:55:56 -0400

    Create files

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,13 @@
 import '../CSS/App.css';
+import Header from './Header';
+import Homepage from './Homepage';
+import Details from './Details';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Homepage />
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../CSS/Header.css"  
+
+function Header() {
+  return (
+    <h1>New Roman, Times</h1>
+  )
+}
+
+export default Header
