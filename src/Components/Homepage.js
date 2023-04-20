441dea89c3618b34b728b2d3c6f3b9cb66a8713e
Angela Wirth <angiewirth92@gmail.com>
2023-04-20 16:52:23 -0400

    Remove console log

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -5,28 +5,28 @@ import "../.env.local"
 
 function Homepage() {
   const [articles, setArticles] = useState([])
+  const [selection, setSelection] = useState("")
+
   useEffect(() => {
-    
     fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=LBazL3jj290PUXHjIAGZYeNb9gyQg2ZX`)
     .then(res => res.json())
     .then(data => {
-      console.log('DATA:', data.results)
       setArticles(data.results)
     })
-
   }, [])
-  
-  
+
   const cards = articles.map((article) => {
     return (
       <Card  
       key={article.uri}
+      id={article.url}
       img={article.multimedia[0].url}
       alt={article.multimedia[0].caption}
       title={article.title}
       />
     )
   })
+
   return (
     <div className="homepage-container">
       {cards}
