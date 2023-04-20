7b193607ffa21150498cc82642022d9a5484695f
Angela Wirth <angiewirth92@gmail.com>
2023-04-20 16:51:34 -0400

    Import Routes

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,16 @@
+import React, {useState, useEffect} from 'react';
 import '../CSS/App.css';
 import Homepage from './Homepage';
+import {Routes, Route, NavLink} from 'react-router-dom'
 
 function App() {
+
   return (
     <div className="App">
       <header>
         <h1>All the Times: Yesterday, Today, Tomorrow</h1>
       </header>
-      <Homepage />
+         <Homepage />
     </div>
   )
 }
