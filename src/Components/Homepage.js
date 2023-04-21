df2c372fc0fd9fe013325cf539e2de7831bcf28b
Angela Wirth <angiewirth92@gmail.com>
2023-04-21 12:11:10 -0400

    Filter articles by category dropdown

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -5,17 +5,25 @@ import "../.env.local"
 
 function Homepage() {
   const [articles, setArticles] = useState([])
-  const [selection, setSelection] = useState("")
+  const [categories, setCategories] = useState([])
+  const [filtered, setFiltered] = useState([])
 
   useEffect(() => {
     fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=LBazL3jj290PUXHjIAGZYeNb9gyQg2ZX`)
     .then(res => res.json())
     .then(data => {
       setArticles(data.results)
+      setCategories(() => data.results.reduce((acc, article) => {
+        if(!acc.includes(article.section)) {
+          acc.push(article.section)
+        }
+        return acc
+      }, []))
     })
+
   }, [])
 
-  const cards = articles.map((article) => {
+  const allArticles = articles.map((article) => {
     return (
       <Card  
       key={article.uri}
@@ -27,9 +35,42 @@ function Homepage() {
     )
   })
 
+  const filteredArticles = filtered.map((article) => {
+    return (
+      <Card  
+      key={article.uri}
+      id={article.url}
+      img={article.multimedia[0].url}
+      alt={article.multimedia[0].caption}
+      title={article.title}
+      />
+    )
+  })
+
+  const options = categories.map((category, index) => {
+    return (
+      <option value={category} key={index}>{category}</option>
+    )
+  })
+
+  const handleChange = (e) => {
+    e.preventDefault()
+    const something = articles.filter(article => article.section === e.target.value)
+    setFiltered(something)
+  }
+  
+  
+
   return (
     <div className="homepage-container">
-      {cards}
+      <div className="options-container">
+        <label>Filter by Category</label>
+        <select id="filter" className="dropdown" onChange={handleChange}>
+          <option value="nothing">all</option>
+          {options}
+        </select>
+      </div>
+      {filtered.length ? filteredArticles : allArticles }
     </div>
   )
 
